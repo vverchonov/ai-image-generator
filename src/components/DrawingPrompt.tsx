15b5a5bf33f3e02c36bdd9030b7cc0e348e3869b
Viacheslav Verchonov <vverchonov@gmail.com>
2025-01-13 21:20:45 -0500

    commit

--- src/components/DrawingPrompt.tsx
+++ src/components/DrawingPrompt.tsx
@@ -15,6 +15,7 @@ const DrawingPrompt = ({ onSubmit }: DrawingPromptProps) => {
     }
   };
 
+  
   return (
     <div className="max-w-2xl mx-auto p-4">
       <div className="bg-white rounded-lg shadow-md p-6">
